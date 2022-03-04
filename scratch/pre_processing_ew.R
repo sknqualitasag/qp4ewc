b957d7ef9192296f30ddb6512c86ed46c19490b3
Sophie Kunz <sophie.kunz@qualitasag.ch>
2022-03-04 16:25:42 +0100

    update NAMESPACE

---
+++ scratch/pre_processing_ew.R
@@ -0,0 +1,35 @@
+### #
+### #
+### #
+### #   Purpose:   Function related to the pre-processing steps for each scenario to run ECOWEIGHT
+### #   started:   2022-03-04 (skn)
+### #
+### # ##################################################################### ###
+
+
+s_sirebreed <- "LM"
+s_prodsystew <- "2"
+s_marketchannel <- "Natura-Beef"
+s_path_directory2create <-"/Users/skn/muku_Ecoweigth/2022/work"
+s_log <- TRUE
+logger <- 'qp4ewc.log'
+s_input_file_literature <- "inst/extdata/input_literature.txt"
+
+qp4ewc::create_directory_scenario(ps_sirebreed = s_sirebreed,
+                          ps_prodsystew = s_prodsystew,
+                          ps_marketchannel = s_marketchannel,
+                          ps_path_directory2create = s_path_directory2create,
+                          pb_log = s_log,
+                          plogger = logger)
+
+
+qp4ewc::read_file_input_literature(ps_input_file_literature,
+                           pb_log = s_log,
+                           plogger = logger)
+
+
+qp4ewc::update_input_parameter_file(ps_path2template_input_parameter_file,
+                            ps_statement2search,
+                            ps_value2update,
+                            pb_log = s_log,
+                            plogger = logger)
