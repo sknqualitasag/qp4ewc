43923dbd3d11d62bee3905bb4981ac4b1467eb68
Sophie Kunz <sophie.kunz@qualitasag.ch>
2022-04-06 13:50:44 +0200

    Implement calculate_mean_liveweight_slaughter and get_constants

---
+++ R/constants.R
@@ -0,0 +1,20 @@
+### #
+### #
+### #
+### #   Purpose:   Function related to constants to use
+### #   started:   2022-04-06 (skn)
+### #
+### # ##################################################################### ###
+
+
+#' @title Get constants
+#'
+#' @description
+#' Return a list with specific constants that are used.
+#'
+#' @export get_constants
+get_constants <- function(){
+  # return list of default constants
+  return(list(vec_dressing_female = 0.56,
+              vec_dressing_male = 0.58))
+}
--- R/progeny_data_flp.R
+++ R/progeny_data_flp.R
@@ -46,11 +46,12 @@ calculate_mean_birthweight <- function(ps_input_flp_tibble,
   }
 
 
-  ### # Different tibble depending on ps_sex
+  ### # Different tibble depending on ps_sex and ps_marketing_channel
   ### # Slaughtercategory for female to consider is RG == 5
   if(ps_sex == "F"){
     tbl_input <- ps_input_flp_tibble %>% dplyr::filter(`Geschlecht Nako` == "F") %>%
                                          dplyr::filter(`Schlacht-/Masttierkategorie` == 5) %>%
+                                         dplyr::filter(Markenprogramm == ps_marketing_channel) %>%
                                          dplyr::select(`Geburtsgewicht Nako`,`ageAtSlaughterInDays`) %>%
                                          na.omit()
     qp4ewc_log_info(lgr, 'calculate_mean_birthweight',
@@ -59,6 +60,7 @@ calculate_mean_birthweight <- function(ps_input_flp_tibble,
     ### # Slaughtercategory for male to consider is OB == 2 and MT == 3
     tbl_input <- ps_input_flp_tibble %>% dplyr::filter(`Geschlecht Nako` == "M") %>%
                                          dplyr::filter(`Schlacht-/Masttierkategorie` == 2 | `Schlacht-/Masttierkategorie` == 3) %>%
+                                         dplyr::filter(Markenprogramm == ps_marketing_channel) %>%
                                          dplyr::select(`Geburtsgewicht Nako`,`ageAtSlaughterInDays`) %>%
                                          na.omit()
     qp4ewc_log_info(lgr, 'calculate_mean_birthweight',
@@ -72,6 +74,95 @@ calculate_mean_birthweight <- function(ps_input_flp_tibble,
                   paste0('Mean birthweight for ',ps_sex,' is : ',birthwt))
 
   return(birthwt)
+
+
+}
+
+
+#' @title Calculate mean live weight at slaughter
+#'
+#' @description
+#' The program package ECOWEIGHT (C Programs for Calculating Economic Weights in Livestock)
+#' need input parameter files. This function will calculate mean live weight at slaughter
+#' based on slaughtercategory, marketing-channel (label of Swiss Beef Cattle Assiocation) and sex.
+#'
+#' @param ps_input_flp_tibble input flp tibble coming from read_file_input_flp in this package
+#' @param ps_sex statement of sex ("F" for female and "M" for male)
+#' @param ps_marketing_channel statement of marketing-channel for Natura-Beef (==2), SwissPrimBeef(==3)
+#' @param pb_log indicator whether logs should be produced
+#' @param plogger logger object
+#'
+#' @importFrom dplyr %>%
+#'
+#' @return mean_liveweight vector
+#'
+#' @export calculate_mean_liveweight_slaughter
+calculate_mean_liveweight_slaughter <- function(ps_input_flp_tibble,
+                                                 ps_sex,
+                                                 ps_marketing_channel,
+                                                 pb_log = FALSE,
+                                                 plogger = NULL){
+
+  ### # Setting the log-file
+  if(pb_log){
+    if(is.null(plogger)){
+      lgr <- get_qp4ewc_logger(ps_logfile = 'calculate_mean_liveweight_slaughter.log',
+                               ps_level = 'INFO')
+    }else{
+      lgr <- plogger
+    }
+    qp4ewc_log_info(lgr, 'calculate_mean_liveweight_slaughter',
+                    paste0('Starting function with parameters:\n * ps_input_flp_tibble \n',
+                           ' * ps_sex: ', ps_sex,'\n',
+                           ' * ps_marketing_channel: ',ps_marketing_channel,'\n'))
+  }
+
+
+  ### # Different tibble depending on ps_sex and ps_marketing_channel
+  ### # Slaughtercategory for female to consider is RG == 5
+  if(ps_sex == "F"){
+    tbl_input <- ps_input_flp_tibble %>% dplyr::filter(`Geschlecht Nako` == "F") %>%
+                                         dplyr::filter(`Schlacht-/Masttierkategorie` == 5) %>%
+                                         dplyr::filter(Markenprogramm == ps_marketing_channel) %>%
+                                         dplyr::select(`Schlachtgewicht Nako`,ageAtSlaughterInDays) %>%
+                                         na.omit()
+    qp4ewc_log_info(lgr, 'calculate_mean_liveweight_slaughter',
+                    paste0('A Tibble for female has been created for the calculation of mean live weight at slaughter '))
+  }else{
+    ### # Slaughtercategory for male to consider is OB == 2 and MT == 3
+    tbl_input <- ps_input_flp_tibble %>% dplyr::filter(`Geschlecht Nako` == "M") %>%
+                                         dplyr::filter(`Schlacht-/Masttierkategorie` == 2 | `Schlacht-/Masttierkategorie` == 3) %>%
+                                         dplyr::filter(Markenprogramm == ps_marketing_channel) %>%
+                                         dplyr::select(`Schlachtgewicht Nako`,ageAtSlaughterInDays) %>%
+                                         na.omit()
+    qp4ewc_log_info(lgr, 'calculate_mean_liveweight_slaughter',
+                    paste0('A Tibble for male has been created for the calculation of mean live weight at slaughter '))
+  }
+
+
+  ### # Calculate the mean carcass weight
+  carcasswt <- round(as.numeric(dplyr::summarise(tbl_input, mean_carcasswt = mean(`Schlachtgewicht Nako`))),4)
+
+
+  ### # Get the constants
+  l_constants <- get_constants()
+
+
+  ### # Calculate the mean live weight at slaughter
+  if(ps_sex == "F"){
+    # dressing percentage to convert carcass weight to live weight at slaughter come from Proviande Wochenpreise für Rindvieh (RG, Fleischigkeit C)
+    livewt_atslaughter <- round(as.numeric((carcasswt/l_constants$vec_dressing_female),4))
+  }else{
+    # dressing percentage to convert carcass weight to live weight at slaughter come from Proviande Wochenpreise für Rindvieh (MT, Fleischigkeit C)
+    livewt_atslaughter <- round(as.numeric((carcasswt/l_constants$vec_dressing_male),4))
+  }
+
+
+  qp4ewc_log_info(lgr, 'calculate_mean_liveweight_slaughter',
+                  paste0('Mean live weight at slaughter for ',ps_sex,' is : ',livewt_atslaughter))
+
+
+  return(livewt_atslaughter)
 
 
 }
