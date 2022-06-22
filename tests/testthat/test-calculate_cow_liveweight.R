8899526b2cf5535059465428be2295a01bc892c1
Sophie Kunz <sophie.kunz@qualitasag.ch>
2022-06-22 11:08:30 +0200

    add ps_first_calvingweight in the test

--- tests/testthat/test-calculate_cow_liveweight.R
+++ tests/testthat/test-calculate_cow_liveweight.R
@@ -14,6 +14,7 @@ test_that("testing function calculate_cow_liveweight()", {
 
   second_calving_wt_expected <- 671
   second_calving_wt <- calculate_cow_liveweight(ps_input_flp_tibble = tbl_flp,
+                                                ps_first_calvingweight = FALSE,
                                                 ps_second_calvingweight = TRUE,
                                                 pb_log = b_log)
   expect_equal(second_calving_wt, second_calving_wt_expected)
@@ -21,6 +22,7 @@ test_that("testing function calculate_cow_liveweight()", {
 
   mature_weight_cow_expected <- 606
   mature_weight_cow <- calculate_cow_liveweight(ps_input_flp_tibble = tbl_flp,
+                                                ps_first_calvingweight = FALSE,
                                                 ps_second_calvingweight = FALSE,
                                                 pb_log = b_log)
   expect_equal(mature_weight_cow, mature_weight_cow_expected)
