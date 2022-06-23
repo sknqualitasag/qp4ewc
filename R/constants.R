d1be26eb8b9735a0446e71a29fd0a119abff6d0f
Sophie Kunz <sophie.kunz@qualitasag.ch>
2022-06-23 17:06:01 +0200

    add function get_constants_carcass_beefOnbeef and optimize pre_process_ew_input_carcass_data_flp with constants

--- R/constants.R
+++ R/constants.R
@@ -7,7 +7,47 @@
 ### # ##################################################################### ###
 
 
-#' @title Get constants
+#' @title Get constants for calving in beef-on-beef production systems
+#'
+#' @description
+#' Return a list with specific calving constants in beef-on-beef production systems
+#' that are used.
+#'
+#' @export get_constants_calving_beefOnbeef
+get_constants_calving_beefOnbeef <- function(){
+  # return list of default constants
+  return(list(idx_col_input_file = 1,
+              idx_col_input = 2,
+              idx_col_input_value = 4,
+              idx_row_abortrate = 1,
+              idx_row_stillborn_easy = 2,
+              idx_row_stillborn_diff = 3,
+              idx_row_class_calving = 4,
+              idx_row_class_dystocia = 5,
+              idx_row_calvingscore2_female = 6,
+              idx_row_calvingscore3_female = 7,
+              idx_row_calvingscore4_female = 8,
+              idx_row_calvingscore2_male = 9,
+              idx_row_calvingscore3_male = 10,
+              idx_row_calvingscore4_male = 11,
+              idx_row_calfdied48h_dystocia = 12,
+              idx_row_calfdied48h_easy = 13,
+              idx_row_calfloss = 14,
+              idx_row_AI_dystocia = 15,
+              idx_row_AI_nodystocia = 16,
+              first_element_vector = 1,
+              second_element_vector = 2,
+              n_class_calving = 4,
+              class_dystocia = 3,
+              sex_female = "F",
+              sex_male = "M",
+              calvingscore2 = 2,
+              calvingscore3 = 3,
+              calvingscore4 = 4))
+}
+
+
+#' @title Get constants for progeny in beef-on-beef production systems
 #'
 #' @description
 #' Return a list with specific constants that are used.
@@ -51,42 +91,41 @@ get_constants_progeny_beefOnbeef <- function(){
 }
 
 
-
-#' @title Get constants for calving in beef-on-beef production systems
+#' @title Get constants for carcass in beef-on-beef production systems
 #'
 #' @description
-#' Return a list with specific calving constants in beef-on-beef production systems
-#' that are used.
+#' Return a list with specific constants that are used.
 #'
-#' @export get_constants_calving_beefOnbeef
-get_constants_calving_beefOnbeef <- function(){
+#' @export get_constants_carcass_beefOnbeef
+get_constants_carcass_beefOnbeef <- function(){
   # return list of default constants
   return(list(idx_col_input_file = 1,
               idx_col_input = 2,
               idx_col_input_value = 4,
-              idx_row_abortrate = 1,
-              idx_row_stillborn_easy = 2,
-              idx_row_stillborn_diff = 3,
-              idx_row_class_calving = 4,
-              idx_row_class_dystocia = 5,
-              idx_row_calvingscore2_female = 6,
-              idx_row_calvingscore3_female = 7,
-              idx_row_calvingscore4_female = 8,
-              idx_row_calvingscore2_male = 9,
-              idx_row_calvingscore3_male = 10,
-              idx_row_calvingscore4_male = 11,
-              idx_row_calfdied48h_dystocia = 12,
-              idx_row_calfdied48h_easy = 13,
-              idx_row_calfloss = 14,
-              idx_row_AI_dystocia = 15,
-              idx_row_AI_nodystocia = 16,
-              first_element_vector = 1,
-              second_element_vector = 2,
-              n_class_calving = 4,
-              class_dystocia = 3,
+              idx_row_cow_freq_class = 1,
+              idx_row_bull_freq_class = 2,
+              idx_row_heifer_freq_class = 3,
+              idx_row_cow_price = 4,
+              idx_row_bull_avprice = 5,
+              idx_row_bull_price = 6,
+              idx_row_heifer_price = 7,
+              idx_row_cow_coef = 8,
+              idx_row_bull_coef = 9,
+              idx_row_heifer_coef = 10,
+              idx_row_class_fleshiness = 11,
+              idx_row_class_fat = 12,
               sex_female = "F",
               sex_male = "M",
-              calvingscore2 = 2,
-              calvingscore3 = 3,
-              calvingscore4 = 4))
+              line1 = 1,
+              line2 = 2,
+              line3 = 3,
+              line4 = 4,
+              line5 = 5,
+              line6 = 6,
+              line7 = 7,
+              line8 = 8,
+              line9 = 9))
 }
+
+
+
