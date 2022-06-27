ea280190944c0fbeb8caff7e21f459037b2e410f
Sophie Kunz <sophie.kunz@qualitasag.ch>
2022-06-27 14:24:47 +0200

    modify calculate_calvingscore_proportion, the calving-vignette, the unit-test and pre_process_ew_input_calving (skn)

--- tests/testthat/test-calculate_calvingscore_proportion.R
+++ tests/testthat/test-calculate_calvingscore_proportion.R
@@ -12,20 +12,23 @@ test_that("testing function calculate_calvingscore_proportion()", {
 
 
   s_sirebreed <- "LM"
+  s_dambreed <- "LM"
   #  Proportion for calving score 2 for females
   calvingscore_prop_prim_F_2_expected <- 0.5
   calvingscore_prop_prim_F_2 <- calculate_calvingscore_proportion(ps_input_calving_tibble = tbl_calving,
                                                                   ps_statement_firstlactation = TRUE,
-                                                                  ps_breed = s_sirebreed,
                                                                   ps_sex = "F",
                                                                   ps_calvingscore = 2,
+                                                                  ps_sirebreed = s_sirebreed,
+                                                                  ps_dambreed = s_dambreed,
                                                                   pb_log = b_log)
   calvingscore_prop_multi_F_2_expected <- 0
   calvingscore_prop_multi_F_2 <- calculate_calvingscore_proportion(ps_input_calving_tibble = tbl_calving,
                                                                    ps_statement_firstlactation = FALSE,
-                                                                   ps_breed = s_sirebreed,
                                                                    ps_sex = "F",
                                                                    ps_calvingscore = 2,
+                                                                   ps_sirebreed = s_sirebreed,
+                                                                   ps_dambreed = s_dambreed,
                                                                    pb_log = b_log)
   # Build a vector with 10 lactations
   value2update_calvingscoreprop_prim_F_2_expected <- paste0(c(calvingscore_prop_prim_F_2_expected, rep(calvingscore_prop_multi_F_2_expected,9)),collapse = " ")
@@ -38,16 +41,18 @@ test_that("testing function calculate_calvingscore_proportion()", {
   calvingscore_prop_prim_M_3_expected <- 0.3333
   calvingscore_prop_prim_M_3 <- calculate_calvingscore_proportion(ps_input_calving_tibble = tbl_calving,
                                                                   ps_statement_firstlactation = TRUE,
-                                                                  ps_breed = s_sirebreed,
                                                                   ps_sex = "M",
                                                                   ps_calvingscore = 3,
+                                                                  ps_sirebreed = s_sirebreed,
+                                                                  ps_dambreed = s_dambreed,
                                                                   pb_log = b_log)
   calvingscore_prop_multi_M_3_expected <- 0
   calvingscore_prop_multi_M_3 <- calculate_calvingscore_proportion(ps_input_calving_tibble = tbl_calving,
                                                                    ps_statement_firstlactation = FALSE,
-                                                                   ps_breed = s_sirebreed,
                                                                    ps_sex = "M",
                                                                    ps_calvingscore = 3,
+                                                                   ps_sirebreed = s_sirebreed,
+                                                                   ps_dambreed = s_dambreed,
                                                                    pb_log = b_log)
   # Build a vector with 10 lactations for calving score 3 proportion for males
   value2update_calvingscoreprop_prim_M_3_expected <- paste0(c(calvingscore_prop_prim_M_3_expected, rep(calvingscore_prop_multi_M_3_expected,9)),collapse = " ")
